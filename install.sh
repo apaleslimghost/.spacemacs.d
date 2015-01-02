9aa970370d4c8c1569af4d7df93825a9de45f713
Matt Brennan <mattyb1000@gmail.com>
2015-01-02 13:48:19 +0000

    icon

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 set -x -e
 ln -sf ~/.spacemacs.d/.spacemacs ~/.spacemacs
+killall Emacs || echo "Emacs not running"
+wget -O /Applications/Emacs.app/Contents/Resources/Emacs.icns https://github.com/cg433n/emacs-yosemite-icon/raw/master/emacs.icns
